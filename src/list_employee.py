221d2e2cf367d638c5c7d92a59f4f1781dc8636a
Layla De Paull <gcas022002@coderacademy.edu.au>
2023-02-06 12:17:10 +1100

    created list_employee file, & list_employee function

---
+++ src/list_employee.py
@@ -0,0 +1,8 @@
+'''A print-out of all employee attributes'''
+def list_employee(employee):
+    print(f"\nName: {employee.name} \n")
+    print(f"Age: {employee.age} \n")
+    print(f"Role: {employee.role} \n")
+    print(f"Salary: {employee.salary} \n")
+
+

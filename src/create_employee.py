146e9bf97f6c294d103fb7b66744a1927d1e58f8
Layla De Paull <gcas022002@coderacademy.edu.au>
2023-02-06 12:27:40 +1100

    added loop to return employee instance, print statement, & list_employee function call to create_employee

--- src/create_employee.py
+++ src/create_employee.py
@@ -4,6 +4,9 @@ from employee import Employee, employee_list
 '''Import file for functions; check_for_valid_string & check_for_valid_number to run'''
 from check_inputs import check_for_valid_string, check_for_valid_number
 
+'''Import file for function list_employees to run'''
+from list_employee import list_employee
+
 '''Allows user to create an entirely new instance of an employee and add it to the employee_list.'''
 def create_employee():
     name = check_for_valid_string("Please enter the employee's name:\n").title() 
@@ -11,3 +14,12 @@ def create_employee():
     role = check_for_valid_string("Please enter the employee's role:\n").title()
     salary = check_for_valid_number("Please enter the employee's salary:\n")
     employee_list.append(Employee(name, age, role, salary))
+
+    '''Return instance of employee just created'''
+    for employee in employee_list:
+        if employee == name:
+            return employee
+
+    '''Confirm to the user the employee instance was created.'''
+    print(f"\nYou have successfully added '{employee.name}' to the Employee Database.\n")
+    list_employee(employee)        

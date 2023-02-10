baf7a0756a0731c62707951fad00e065e28e59d7
Layla De Paull <gcas022002@coderacademy.edu.au>
2023-02-10 12:54:42 +1100

    altered check_for_valid_string function; added only_letters function for regex check of string inputs

--- src/check_inputs.py
+++ src/check_inputs.py
@@ -18,7 +18,7 @@ def check_for_valid_string(message):
                 raise ValueError("You did not make an entry, please try again.")
             except ValueError as warning:
                 print(f"{warning}\n")     
-        elif entry.isdigit():
+        elif only_letters(entry) == False:
             entry = ""
             try: 
                 raise ValueError("You entered a number, please enter a name this time.")
@@ -26,3 +26,6 @@ def check_for_valid_string(message):
                 print(f"{warning}\n")
         else:
             return entry
+
+def only_letters(string):
+    return bool(re.match("^[a-zA-Z_ ]+$", string))            

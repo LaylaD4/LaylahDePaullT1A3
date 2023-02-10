7ca9e26990e06f855a1afdb35f33447c18af507a
Layla De Paull <gcas022002@coderacademy.edu.au>
2023-02-10 22:25:51 +1100

    changed colour of output for list_employee function

--- src/list_employee.py
+++ src/list_employee.py
@@ -5,9 +5,9 @@ colorama.init(autoreset=True)
 
 '''A print-out of all employee attributes'''
 def list_employee(employee):
-    print(f"\n{Fore.CYAN}Name:{Fore.BLACK}{Style.BRIGHT} {employee.name}\n")
-    print(f"{Fore.CYAN}Age:{Fore.BLACK}{Style.BRIGHT} {employee.age}\n")
-    print(f"{Fore.CYAN}Role:{Fore.BLACK}{Style.BRIGHT} {employee.role}\n")
-    print(f"{Fore.CYAN}Salary:{Fore.BLACK}{Style.BRIGHT} {employee.salary}\n")
+    print(f"\n{Fore.BLUE}Name:{Fore.BLACK}{Style.BRIGHT} {employee.name}\n")
+    print(f"{Fore.BLUE}Age:{Fore.BLACK}{Style.BRIGHT} {employee.age}\n")
+    print(f"{Fore.BLUE}Role:{Fore.BLACK}{Style.BRIGHT} {employee.role}\n")
+    print(f"{Fore.BLUE}Salary:{Fore.BLACK}{Style.BRIGHT} {employee.salary}\n")
 
 

b8d929e8586b61840f7f09e98cd138ce768977a7
Layla De Paull <gcas022002@coderacademy.edu.au>
2023-02-10 15:11:23 +1100

    created handle_number_errors function, altered check_for_valid_number to handle for inputs over 18, & 42000 using handle_numbers...function call

--- src/check_inputs.py
+++ src/check_inputs.py
@@ -2,12 +2,16 @@
 import re 
 
 '''Checks that input from user is a number character, and handles float inputs.'''
-def check_for_valid_number(message):
+def check_for_valid_number(message, number_type):
     number = None
     while number == None:
         try:
             number = float(input(message))
-            return int(number)
+            number = handle_number_errors(number, number_type)
+            if number == False:
+                return check_for_valid_number(message, number_type)
+            else:    
+                return number
         except ValueError:
             print("That is not a number. Please try again.\n")
 
@@ -30,5 +34,20 @@ def check_for_valid_string(message):
         else:
             return entry
 
+'''Checks that only letters are entered using regex, and re import'''
 def only_letters(string):
-    return bool(re.match("^[a-zA-Z_ ]+$", string))            
+    return bool(re.match("^[a-zA-Z_ ]+$", string)) 
+
+'''Checks that any age or salary entered is over 18, and 42000 respectively'''
+def handle_number_errors(number, number_type):
+    number = int(number)
+    if number >= 18 and number_type == "age":
+        return number 
+    elif number >= 42000 and number_type == "salary":
+        return number 
+    else:
+        if number_type == "age":
+            print("Please enter an age that is: 18 or above.\n")
+        else:
+            print("Please enter a salary that is: 42000 (minimum wage) or above.\n")
+        return False               

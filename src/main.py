1aaacd70e73c59f9b8901683af840596ff9bf6da
Layla De Paull <gcas022002@coderacademy.edu.au>
2023-02-15 19:22:10 +1100

    added shell command clear + clean up code

--- src/main.py
+++ src/main.py
@@ -1,43 +1,47 @@
-'''This python library allows for a list function output for the user to use arrow keys to move up & down menu items.'''
+# This python library allows for a list function output for the user to use arrow keys to move up & down menu items.
 import inquirer
 
-'''This allows for coloured text output in the terminal.'''
+# This allows for coloured text output in the terminal.
 import colorama
 from colorama import Fore, Back, Style
 colorama.init(autoreset=True)
 
-'''Import file for function: create_employee to run.'''
+# This allows for shell commands in the terminal
+import os
+
+# Import file for function: create_employee to run.
 from create_employee import create_employee
 
-'''Import file for function: list_all_employees to run'''
+# Import file for function: list_all_employees to run
 from list_all_employees import list_all_employees
 
-'''Import file for function: create_employee to run.'''
+# Import file for function: create_employee to run.
 from edit_employee import edit_employee
 
-'''Import file for function: list_all_employees to run'''
+# Import file for function: list_all_employees to run
 from delete_employee import delete_employee
 
-'''Import file for function: list_one_employee to run.'''
+# Import file for function: list_one_employee to run.
 from list_one_employee import list_one_employee
 
-'''Import file for function: data_statistics_search to run'''
+# Import file for function: data_statistics_search to run.
 from data_statistics_search import data_statistics_search
 
-'''Import file for function: print_employee_list to run'''
+# Import file for function: print_employee_list to run'''
 from print_employee_list import print_employee_list
 
-'''Centres output in terminal'''
+# Centres output in terminal
 from print_centre import print_centre
 
-'''Alows for bash script input'''
+# Alows for bash script argument
 from sys import argv
 
-'''Import datetime module to get current date.'''
+# Import datetime module to get current date.
 from datetime import date
 
-'''Returns full name from bash script'''
+
 def get_full_name():
+    """Returns full name from bash script argument"""
     i = 1
     name = ""
     while i < len(argv):
@@ -45,13 +49,14 @@ def get_full_name():
         i = i + 1
     return name
 
-# Log in details:
-print(f"\n{Fore.BLACK}{Style.BRIGHT}Logged In: {Fore.BLUE}{get_full_name().title()} \n{Fore.BLACK}{Style.BRIGHT}Date: {Fore.BLUE}{date.today()}")    
-
-print("\n")
-print_centre(f"{Fore.GREEN}{Style.BRIGHT}Hello {Fore.MAGENTA}{get_full_name().title()}{Fore.GREEN}Welcome to the 'Initech' Employee Database Application.")
 
 def main_menu():
+    """Creates the MAIN MENU options together with external python library inquirer."""
+    os.system('clear')
+    print(f"\n{Fore.BLACK}{Style.BRIGHT}Logged In: {Fore.BLUE}{get_full_name().title()} \n{Fore.BLACK}{Style.BRIGHT}Date: {Fore.BLUE}{date.today()}")    
+
+    print("\n")
+    print_centre(f"{Fore.GREEN}{Style.BRIGHT}Hello {Fore.MAGENTA}{get_full_name().title()}{Fore.GREEN}Welcome to the 'Initech' Employee Database Application.")
     print("\n")
     print_centre(f"{Fore.MAGENTA}MAIN MENU") 
     print("\n")
@@ -60,29 +65,39 @@ def main_menu():
     inquirer.List(
         "Menu Options",
         message=f"{Fore.CYAN}Please select from the following options using the arrow keys, and press enter.",
-        choices=["Display all Employees", "List an Employee", "Create an Employee", "Edit an existing Employee", "Delete an existing Employee", "Employee Data Statistics", "Print Employee List to File", "Exit Database"],)]
+        choices=["Display all Employees", "List an Employee", "Create an Employee", "Edit an Existing Employee", "Delete an Existing Employee", "Employee Data Statistics", "Print Employee List to File", "Exit Database"],)]
     answers = inquirer.prompt(questions)
     
     if answers["Menu Options"] == "Display all Employees":
+        os.system('clear')
         list_all_employees()
     if answers["Menu Options"] == "List an Employee":
+        os.system('clear')
         list_one_employee()
     if answers["Menu Options"] == "Create an Employee":
+        os.system('clear')
         create_employee() 
-    if answers["Menu Options"] == "Edit an existing Employee":
+    if answers["Menu Options"] == "Edit an Existing Employee":
+        os.system('clear')
         edit_employee()
-    if answers["Menu Options"] == "Delete an existing Employee":
+    if answers["Menu Options"] == "Delete an Existing Employee":
+        os.system('clear')
         delete_employee()
     if answers["Menu Options"] == "Employee Data Statistics":
+        os.system('clear')
         data_statistics_search()
     if answers["Menu Options"] == "Print Employee List to File":
+        os.system('clear')
         print_employee_list()
-    if answers["Menu Options"] == "Exit Database":  
+    if answers["Menu Options"] == "Exit Database":
+        os.system('clear')  
         print(f"\n{Fore.BLACK}{Style.BRIGHT}Logged Out: {Fore.BLUE}{get_full_name().title()} \n{Fore.BLACK}{Style.BRIGHT}Date: {Fore.BLUE}{date.today()}\n")
         exit()    
     else:
         main_menu()
 
-
 main_menu()
+
+
+
 
